7282043b56ac5735858a59035d75d8fe68372a47
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-03-18 03:23:31 -0700

    liv vault contracts

--- src/config/constants/vaults.ts
+++ src/config/constants/vaults.ts
@@ -14,7 +14,7 @@ const vaults: SerializedVaultConfig[] = [
         },
         contractAddresses: {
             97: '0xdf075e54Fc6E7F114399c511E621EA2fe999C451',
-            56: '0x0e587eaeFC234965ef5b2219983E7Df06b42dAE3',
+            56: '0x44425C94Df7cF5916E8B9497C99a4584bE9a912e',
         },
         isETH: false,
         token: serializedTokens.busd,
@@ -29,7 +29,7 @@ const vaults: SerializedVaultConfig[] = [
         },
         contractAddresses: {
             97: '0x17e2Ef101b2A0f71e11eFa2B353B1D67a1546222',
-            56: '0x0e587eaeFC234965ef5b2219983E7Df06b42dAE3',
+            56: '0x1BAf386a4305f2Cb4aFF27456efCcbC21fcc6c38',
         },
         isETH: true,
         token: serializedTokens.wbnb,
@@ -40,11 +40,11 @@ const vaults: SerializedVaultConfig[] = [
         lpSymbol: 'SPY-USDC LP',
         lpAddresses: {
             97: '0x1aec329c825fA6dE8be4f0CB5410C0D4650dACE9',
-            56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+            56: '0xc115f337130aaaE97F4Ab34751ee2854D2795958',
         },
         contractAddresses: {
             97: '0x54580DD184BF9201fB0e2ACf0Ce801dd3CA045e7',
-            56: '0x0e587eaeFC234965ef5b2219983E7Df06b42dAE3',
+            56: '0xcb1E24339d157695468072c1452BFb79bCae45b9',
         },
         isETH: false,
         token: serializedTokens.usdc,
