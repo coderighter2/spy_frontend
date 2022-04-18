8df6f780d98ae82dec2251347581a2f616cb03aa
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-04-18 11:01:23 -0700

    disable harvest on old farms

--- src/views/Farms/components/FarmCard/HarvestAction.tsx
+++ src/views/Farms/components/FarmCard/HarvestAction.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import BigNumber from 'bignumber.js'
-import { Button, Flex, Heading } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+import { Box, Button, Flex, Heading, HelpIcon, useTooltip } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { useAppDispatch } from 'state'
 import { fetchFarmUserDataAsync, fetchOldFarmUserDataAsync } from 'state/farms'
@@ -12,6 +13,9 @@ import { usePriceCakeBusd } from 'state/farms/hooks'
 import Balance from 'components/Balance'
 import useHarvestFarm from '../../hooks/useHarvestFarm'
 
+const ReferenceElement = styled.div`
+  display: inline-block;
+`
 interface FarmCardActionsProps {
   earnings?: BigNumber
   pid?: number
@@ -30,6 +34,10 @@ const HarvestAction: React.FC<FarmCardActionsProps> = ({ isOld, earnings, pid, n
   const rawEarningsBalance = account ? getBalanceAmount(earnings, 0) : BIG_ZERO
   const displayBalance = rawEarningsBalance.toFixed(3, BigNumber.ROUND_DOWN)
   const earningsBusd = rawEarningsBalance ? rawEarningsBalance.multipliedBy(cakePrice).toNumber() : 0
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(
+    t('Harvest / Stake is stopped working, please migrate your tokens to the main pools'),
+    { placement: 'top-end', tooltipOffset: [20, 10] },
+  )
 
   return (
     <Flex mb="8px" justifyContent="space-between" alignItems="center">
@@ -40,7 +48,7 @@ const HarvestAction: React.FC<FarmCardActionsProps> = ({ isOld, earnings, pid, n
         )}
       </Flex>
       <Button
-        disabled={rawEarningsBalance.eq(0) || pendingTx || !nextHarvestUntil || nextHarvestUntil === 0  || nextHarvestUntil > Math.floor(Date.now() / 1000) }
+        disabled={isOld || rawEarningsBalance.eq(0) || pendingTx || !nextHarvestUntil || nextHarvestUntil === 0  || nextHarvestUntil > Math.floor(Date.now() / 1000) }
         onClick={async () => {
           setPendingTx(true)
           try {
@@ -65,8 +73,17 @@ const HarvestAction: React.FC<FarmCardActionsProps> = ({ isOld, earnings, pid, n
           }
         }}
       >
-        {pendingTx ? t('Harvesting') : t('Harvest')}
+        <Flex justifyContent="center" alignItems="center">
+          <span>{pendingTx ? t('Harvesting') : t('Harvest')}</span>
+          {isOld && (
+            <Box ref={targetRef} ml="8px">
+              <HelpIcon 
+                color="textSubtle" />
+            </Box>
+          )}
+        </Flex>
       </Button>
+      {tooltipVisible && tooltip}
     </Flex>
   )
 }
